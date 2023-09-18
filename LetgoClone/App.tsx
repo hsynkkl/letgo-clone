6b643cc79114cb0429235877e8e138d384537499
hsyn <kak.hus@hotmail.com>
2023-09-18 21:33:45 +0300

    22:46 p3

--- LetgoClone/App.tsx
+++ LetgoClone/App.tsx
@@ -3,15 +3,20 @@ import { StyleSheet, Text, View, LogBox } from "react-native";
 import HomeScreen from "../LetgoClone/src/screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigators/RootNavigator";
+import { Amplify } from "aws-amplify";
+import awsExports from "./src/aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native";
+
+Amplify.configure(awsExports);
 LogBox.ignoreAllLogs();
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
   );
 }
-
+export default withAuthenticator(App);
 const styles = StyleSheet.create({
   container: {
     marginTop: "15%",
---
+++ LetgoClone/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "letgoclone85e67b03": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
